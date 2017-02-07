f93bb7d49018766dd39ad7131476b043c6fe8962
IOhannes m zmölnig <zmoelnig@iem.at>
2017-02-07 13:25:12 +0100

    script for single-test runs
    so we can call it better from 'make check'

---
+++ tests/run1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PD=${PD:=pd}
+LIBDIR=${LIBDIR:=../src/.libs/}
+SRCDIR=${SRCDIR:=../}
+
+
+PD=$(which ${PD})
+TEST=$1
+
+if [ "x${PD}" = "x" ]; then
+ exit 77
+fi
+
+LIBFLAGS="-path ${LIBDIR} -lib zexy -path ${SRCDIR}/abs:."
+TMPFILE=$(mktemp)
+
+${PD} \
+	-noprefs -oss -nosound \
+	-nogui -batch \
+	${LIBFLAGS} \
+	-open run1.pd \
+	-send "test ${TEST%.pd}" 2>&1 \
+	| tee "${TMPFILE}"
+
+
+egrep "^regression-test: ${TEST%.pd}: OK" "${TMPFILE}" >/dev/null
+SUCCESS=$?
+
+rm "${TMPFILE}"
+
+exit ${SUCCESS}
